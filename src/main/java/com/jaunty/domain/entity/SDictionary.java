74fc78c11ad0d29e2e11ec80b12be7e510496603
十年恒 <156815036@139.com>
2019-12-11 21:25:11 +0800

    1、菜单表代码生成；2、字典表代码生成；

--- src/main/java/com/jaunty/domain/entity/SDictionary.java
+++ src/main/java/com/jaunty/domain/entity/SDictionary.java
@@ -1,10 +1,7 @@
 package com.jaunty.domain.entity;
 
-import lombok.Data;
-
 import java.util.Date;
 
-@Data
 public class SDictionary {
     private Long id;
 
@@ -26,4 +23,83 @@ public class SDictionary {
 
     private Date updateTime;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getDicCode() {
+        return dicCode;
+    }
+
+    public void setDicCode(String dicCode) {
+        this.dicCode = dicCode == null ? null : dicCode.trim();
+    }
+
+    public String getDicValue() {
+        return dicValue;
+    }
+
+    public void setDicValue(String dicValue) {
+        this.dicValue = dicValue == null ? null : dicValue.trim();
+    }
+
+    public Byte getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Byte seq) {
+        this.seq = seq;
+    }
+
+    public Boolean getDflag() {
+        return dflag;
+    }
+
+    public void setDflag(Boolean dflag) {
+        this.dflag = dflag;
+    }
+
+    public Long getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Long createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(Long updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 }
--- src/main/java/com/jaunty/domain/entity/SMenu.java
+++ src/main/java/com/jaunty/domain/entity/SMenu.java
@@ -1,10 +1,7 @@
 package com.jaunty.domain.entity;
 
-import lombok.Data;
-
 import java.util.Date;
 
-@Data
 public class SMenu {
     private Long id;
 
@@ -14,7 +11,7 @@ public class SMenu {
 
     private String menuName;
 
-    private String menuIcon;
+    private String icon;
 
     private Byte seq;
 
@@ -32,4 +29,107 @@ public class SMenu {
 
     private Date updateTime;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getMenuType() {
+        return menuType;
+    }
+
+    public void setMenuType(String menuType) {
+        this.menuType = menuType == null ? null : menuType.trim();
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName == null ? null : menuName.trim();
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+
+    public Byte getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Byte seq) {
+        this.seq = seq;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions == null ? null : permissions.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Boolean getDflag() {
+        return dflag;
+    }
+
+    public void setDflag(Boolean dflag) {
+        this.dflag = dflag;
+    }
+
+    public Long getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Long createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(Long updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 }
